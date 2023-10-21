9b81be79391e9a2fe8ad9bbaeff8492be244d56b
unknown <nadhir.benslema@esprit.tn>
2023-10-22 00:25:57 +0100

    integ

---
+++ api-gateway/src/main/java/com/example/apigateway/SpringCloudConfig.java
@@ -0,0 +1,30 @@
+package com.example.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/themes/**")
+                        .uri("http://localhost:8088/")
+                )
+
+//Micro-service 2
+                .route(r -> r.path("/ms/recette/**")
+                    .uri("http://localhost:8086/")
+                     )
+                .route(r -> r.path("/api/profile/**")
+                        .uri("http://localhost:8088/")
+                ).build();
+
+
+
+    }
+
+}
