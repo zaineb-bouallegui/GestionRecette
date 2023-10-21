71be9f6f11f5d0766c8a0598f2d98ae798c13cb0
AymenMzoughi <aymen.mzoughi@esprit.tn>
2023-10-21 16:02:07 +0100

    Organizing the push

--- plat/plat/src/main/java/com/esprit/plat/repositories/PlanificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.esprit.plat.repositories;
-
-import com.esprit.plat.entities.Planification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanificationRepository extends JpaRepository<Planification, Long> {
-}
--- plat/plat/src/main/java/com/esprit/plat/repositories/PlatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.esprit.plat.repositories;
-
-import com.esprit.plat.entities.Plat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlatRepository extends JpaRepository<Plat, Long> {
-    // Vous pouvez ajouter des méthodes personnalisées ici si nécessaire
-}
